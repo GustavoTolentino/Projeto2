31c71af6f2fe37d91e9dd00fd259a93b2b17c230
TolentinoGustavo <tolentinogustavo@gmail.com>
2020-11-19 10:28:00 -0300

    projeto tabuada  pronto

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Projeto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deseja saber a tabuada de que número?: ");
+            int resposta = int.Parse(Console.ReadLine());
+
+            int resultado;
+
+            for(int contador = 10; contador> 0; contador--){
+                resultado = resposta * contador;
+                Console.WriteLine($"{resposta} * {contador} = {resultado} ");
+            }
+        }
+    }
+}
